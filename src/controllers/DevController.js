d189b0f9d9820b90d7dc5bb86bdc648705b8a21a
matheusmelo <ma_hrm@hotmail.com>
2020-01-22 18:13:11 -0300

    Project complete

---
+++ src/controllers/DevController.js
@@ -0,0 +1,53 @@
+const axios = require('axios');
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections, sendMessage } = require('../websocket');
+
+module.exports = {
+
+    async index(request, response){
+        const devs = await Dev.find();
+
+        return response.json(devs);
+    },
+
+    async store(request, response){
+        const { github_username, techs, latitude, longitude } = request.body;
+
+        let dev = await Dev.findOne({github_username});
+
+        if(!dev){
+            const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+        
+            const { name = login, avatar_url, bio } = apiResponse.data;
+        
+            const techs_array = parseStringAsArray(techs);
+        
+            const location = {
+                type: 'Point',
+                coordinates: [longitude, latitude]
+            }
+        
+            dev = await Dev.create({
+                github_username,
+                name,
+                avatar_url,
+                bio,
+                techs: techs_array,
+                location
+            });
+
+            // Filtrar conexões que estão há no máximo 10km de distância
+            // E que o novo dev tenha pelo menos uma das tecnologias filtradas
+
+            const sendSocketMessageTo = findConnections(
+                { latitude, longitude },
+                techs_array
+            );
+
+            sendMessage(sendSocketMessageTo, 'new-dev', dev);
+        }
+
+        return response.json(dev);
+    }
+}
